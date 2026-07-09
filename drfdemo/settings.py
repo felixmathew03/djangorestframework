bcf49cf0a8ae1e51e74852a81bd843e30c66346c
felixmathew03 <felixmathewtt@gmail.com>
2026-07-09 09:28:45 +0530

    bug fix

--- drfdemo/settings.py
+++ drfdemo/settings.py
@@ -42,8 +42,8 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':(
-        'rest_framework_simplejwt.authentication.JWTAuthentication'
-    )
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }
 
 MIDDLEWARE = [
